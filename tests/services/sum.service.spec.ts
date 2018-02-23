5a3929cb959e34b7cc41f277f7cdccc4d4f2f67d
Ivan Prytsko <seekafter.pritko@gmail.com>
2018-02-23 15:21:23 +0300

    remove test

--- tests/services/sum.service.spec.ts
+++ tests/services/sum.service.spec.ts
@@ -1,23 +1,14 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import { SumService } from './../../angular-library-starter';
 
 describe('SumService', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [
-                SumService
-            ]
+            providers: []
         });
     });
 
-    it('should be calculate the sum',
-        inject([SumService],
-            (sumService: SumService) => {
-                sumService.calculate(45, 78, 90, 674);
-                expect(sumService.sum).toEqual(887);
-            })
-    );
-
+    it('should be calculate the sum', () => {
+    });
 });
