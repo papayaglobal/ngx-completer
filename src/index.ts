4e45fe7697ede5f24a383ccb5f779415e3a9593d
AlexG <liohaharakh@gmail.com>
2018-03-06 14:21:08 +0300

    added NgxSelectComponent export to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Public classes.
 export { NgxSelectBoxModule } from './modules/ngx-select-box.module';
 export { NgxSelectModule } from './modules/ngx-select/ngx-select.module';
-export { NgxSelectModel } from './modules/ngx-select/component';
+export { NgxSelectModel, NgxSelectComponent } from './modules/ngx-select/component';
