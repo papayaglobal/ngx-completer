7ee0d3056fd44cb29178799f5fd738954c32c271
AlexG <liohaharakh@gmail.com>
2018-03-28 18:02:46 +0300

    hot fix:
     - fixed build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Public classes.
 export { NgxSelectBoxModule } from './modules/ngx-select-box.module';
 export { NgxSelectModule } from './modules/ngx-select/ngx-select.module';
-export { NgxSelectModel, NgxSelectComponent } from './modules/ngx-select/component';
+export { NgxSelectModel } from './modules/ngx-select/component/ngx-select-model';
+export { NgxSelectComponent } from './modules/ngx-select/component/ngx-select.component';
